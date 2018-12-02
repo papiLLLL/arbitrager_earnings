ec54df7eac17bd217c96d0cf9288c1e83e1b6e49
yuitoku <exrecord160@gmail.com>
2018-12-02 18:24:01 +0900

    modify chart js options

--- app/assets/javascripts/earnings.js
+++ app/assets/javascripts/earnings.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(event) {
+  /* Chart.js setting */
+  Chart.defaults.global.defaultFontColor = "#73879C";
+  Chart.defaults.global.defaultFontFamily = "Quicksand, '游ゴシック Medium', 'Yu Gothic Medium', 游ゴシック体, 'Yu Gothic', YuGothic, 'ヒラギノ角ゴシック Pro', 'Hiragino Kaku Gothic Pro', メイリオ, Meiryo, Osaka, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif";
+  
+
   const ctx1 = document.getElementById("jpy_balance").getContext("2d");
   const jpy_balance = new Chart(ctx1, {
     type: "line",
@@ -6,8 +11,22 @@ document.addEventListener('DOMContentLoaded', function(event) {
       labels: gon.created_on,
       datasets: [{
         data: gon.total_balance,
-        borderColor: "rgba(255, 50, 132, 1.0)",
-        fill: false,
+        borderColor: "#CCB3DD",
+        pointBackgroundColor: "#CCB3DD",
+        yAxisID: "total",
+        fill: false
+      }, {
+        data: gon.coincheck_jpy_balance,
+        borderColor: "#91DBB9",
+        pointBackgroundColor: "#91DBB9",
+        yAxisID: "each",
+        fill: false
+      }, {
+        data: gon.quoinex_jpy_balance,
+        borderColor: "#97D3E3",
+        pointBackgroundColor: "#97D3E3",
+        yAxisID: "each",
+        fill: false
       }]
     },
     options: {
@@ -15,6 +34,30 @@ document.addEventListener('DOMContentLoaded', function(event) {
       maintainAspectRatio: false,
       legend: {
         display: false
+      },
+      scales: {
+        yAxes: [{
+          id: "each",
+          type: "linear",
+          position: "left",
+          ticks: {
+            max: 255000,
+            min: 225000,
+            stepSize: 5000,
+          }
+        }, {
+          id: "total",
+          type: "linear",
+          position: "right",
+          ticks: {
+            max: 770000,
+            min: 730000,
+            stepSize: 10000,
+          },
+          gridLines: {
+            drawOnChartArea: false,
+          }
+        }]
       }
     }
   });
