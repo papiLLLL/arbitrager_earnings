f2d51e3ae0917ea3fdae0bca35ad715f058586ec
yuitoku <exrecord160@gmail.com>
2018-12-06 21:09:06 +0900

    modify

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -24,13 +24,7 @@ class EarningsController < ApplicationController
 
     gon.created_on = ei.pluck(:created_on).uniq
     gon.coincheck_jpy_balance = ei.where("name = 'Coincheck'").pluck(:jpy_balance)
-    @coincheck_btc_balance = ei.where("name = 'Coincheck'").pluck(:btc_balance)
-    @coincheck_btc_price = ei.where("name = 'Coincheck'").pluck(:btc_price)
-    gon.coincheck_btc_price = @coincheck_btc_price
     gon.quoinex_jpy_balance = ei.where("name = 'Quoinex'").pluck(:jpy_balance)
-    @quoinex_btc_balance = ei.where("name = 'Quoinex'").pluck(:btc_balance)
-    @quoinex_btc_price = ei.where("name = 'Quoinex'").pluck(:btc_price)
-
     gon.total_balance = pr.pluck(:total_balance)
   end
 
